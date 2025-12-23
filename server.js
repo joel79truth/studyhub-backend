878d3f1e5585785b8b0a9c83c3df50df1296196c
joel79truth <joeltruth165@gmail.com>
2025-12-23 09:38:32 +0200

    this

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ app.use(express.static("public"));
 
 // ------------------ FIREBASE ADMIN (FCM) ------------------
 
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf-8')
+);
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
+
 
 
 // Store tokens in memory (OK for now)
